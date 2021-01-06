bab235887143ae420e822f4c39d1e0b63bb9038e
Bannings <zhangao0086@gmail.com>
2021-01-06 20:24:45 +0800

    改善脚手架

--- post_cil.py
+++ post_cil.py
@@ -3,75 +3,38 @@
 
 __author__ = "Bannings"
 
-import os, re, datetime
+import os, sys, datetime
 
 DATE_FORMAT = '%Y-%m-%d'
 TIME_FORMAT = "%H:%M:%S +0800"
 
-def start():
+def start(filename):
     posts_dir = os.path.join(os.path.dirname(__file__), '_posts')
 
-    # 获取当前最新的 arts post
-    files = sorted(os.listdir(posts_dir))
-    regex = re.compile('(.*?)-ARTS #(\d+)')
-    pre_date, number = None, None
-    for i in range(len(files)-1,-1,-1):
-        if os.path.isfile(os.path.join(posts_dir, files[i])):
-            result = regex.search(files[i])
-            if result:
-                datetime_string, number = result.groups()
-                pre_date = datetime.datetime.strptime(datetime_string, DATE_FORMAT)
-                break
-    
     # 创建新的文件名
-    new_date, new_number = pre_date + datetime.timedelta(days=7), str(int(number) + 1)
-    new_date_string = new_date.strftime(DATE_FORMAT)
-    filename = f'{new_date_string}-ARTS #{new_number}.md'
+    new_date_string = datetime.datetime.today().strftime(DATE_FORMAT)
+    filename = f'{new_date_string}-{filename}.md'
 
     # 创建新的 post
     time_string = datetime.datetime.today().strftime(TIME_FORMAT)
     post_date = f"{new_date_string} {time_string}"
     with open(os.path.join(posts_dir, filename), 'w') as file:
-        file.write(template_content(new_number, post_date))
+        file.write(template_content(post_date))
 
-def template_content(number, date) -> str:
+def template_content(date) -> str:
     return f'''---
 layout: post
-title: "ARTS #{number}"
+title: ""
 date: {date}
-categories: [ARTS]
+categories: [分享]
 article_type: 1
 excerpt_separator: <!--more-->
 typora-root-url: ../../github.io
 ---
 
-
-# Algorithm
-
-本周选择的算法题是：[]()
-
 <!--more-->
-
-## 规则
-
-
-
-## Solution
-
-
-
-
-# Review
-
-
-
-# Tip
-
-
-
-# Share
 
 '''
 
 if __name__ == '__main__':
-    start()
+    start(sys.argv[1])
